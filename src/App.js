aedcd78d7c964fd056591bd74d566860424ae4e8
myth <myth@yahoo.com>
2018-06-25 20:31:18 +0300

    comm2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'tachyons';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">3h Timer</h1>
         </header>
         <p className="App-intro">
           To get started, b edit <code>src/App.js</code> and save to reload.
