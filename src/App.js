b6cec72128ffd3e38aac9ac5eb4e48208008f459
myth <myth@yahoo.com>
2018-06-26 13:49:37 +0300

    comm css html

--- src/App.js
+++ src/App.js
@@ -3,15 +3,24 @@ import './App.css';
 import 'tachyons';
 
 class App extends Component {
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">3h Timer</h1>
-        </header>
-        <p className="App-intro">
-          To get started, b edit <code>src/App.js</code> and save to reload.
-        </p>
+        <body onload="startTime()">
+            <h1>3h Timer</h1>
+            <hr /><br />
+            <button onclick="timer()" class="button" id="button">
+            3 Hour Timer</button>
+            <button onclick="undisableBtn()" class="buttonR">
+            Reset</button><br />
+            <p id="date"></p>
+            <p id="hour"></p><br />
+            <p id="disp1">Start task: </p><br />
+            <p id="disp2">End task: </p><br />
+            <p id="disp3">Task waiting...</p>
+        </body>
       </div>
     );
   }
