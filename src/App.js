9f0a9073c5673a9d3907cbdbbd7302c299088fc1
myth <myth@yahoo.com>
2018-06-29 12:44:59 +0300

    test1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'tachyons';
-import './The Endeavor Begins.mp3';
+import './audio.wav';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      time: new Date().toLocaleTimeString()
-    };
+
+  state = {
+    time: new Date().toLocaleTimeString(),
+    disp1: "Start task:",
+    disp2: "End task:",
+    disp3: "Task waiting...",
+    button: false,
+    bg: "#4CAF50",
+    bgd: "#75507B"
   }
+
   componentDidMount() {
     this.intervalID = setInterval(
       () => this.tick(),
@@ -24,52 +29,65 @@ class App extends Component {
       time: new Date().toLocaleTimeString()
     });
   }
-
   reloadBtn() {
     window.location.reload();
   }
 
-  timer() {
-
-    document.getElementById("button").style.backgroundColor = "gray";
+  timer = () => {
+    // document.getElementById("button").style.backgroundColor = "gray";
+    this.setState({ bg: "gray" });
     const today = new Date();
     const h = today.getHours();
     const m = today.getMinutes();
-
-    document.getElementById('disp1').innerHTML = "Start task: " + h + ":" + m;
+    this.setState({ disp1: "Start task: " + h + ":" + m });
     var h2 = today.getHours() + 3;
-    document.getElementById('disp2').innerHTML = "End task: " + h2 + ":" + m;
-    document.getElementById("disp3").innerHTML = "Task running...";
-    document.getElementById("disp3").style.backgroundColor = "purple";
-    document.getElementById("button").disabled = true;
-    
+    this.setState({ disp2: "End task: " + h2 + ":" + m });
+    this.setState({ disp3: "Task running..." });
+    // document.getElementById("disp3").style.backgroundColor = "purple";
+    this.setState({ bgd: "purple" });
+    // document.getElementById("button").disabled = true;
+    this.setState({ button: true });
+    const c = () => { this.setState({ disp3: "Task finished!!" }); };
+    const d = () => { this.setState({ bgd: "teal" }); }
+    const e = () => {
+      // var audio = new Audio('./audio.wav');
+      // var audio = document.getElementById("audio.wav");
+      // console.log(audio.toString());ser
+      // audio.play();
+    }
     // var tT = 3 * 60 * 60 * 1000;
     // setTimeout(myTimer, 10800000);
     setTimeout(myTimer, 6000);
-
     function myTimer() {
-      document.getElementById("disp3").innerHTML = "Task finished!!";
-      document.getElementById("disp3").style.backgroundColor = "teal";
-      var audio = new Audio('The Endeavor Begins.mp3');
-      console.log(audio.toString());
+      c();
+      // document.getElementById("disp3").style.backgroundColor = "teal";
+      d();
+      e();
+      // var audio = new Audio('The Endeavor Begins.mp3');
+      // console.log(audio.toString());
       // audio.play();
     }
   }
-
   render() {
     return (
       <div className="App">
         <h1>3h Timer</h1>
         <hr /><br />
-        <button onClick={this.timer} id="button">
+        <button onClick={this.timer} id="button"
+          disabled={this.state.button} style={{ background: this.state.bg }}>
           3 Hour Timer</button>
         <button onClick={this.reloadBtn} id="buttonR">
           Reload</button><br />
         <p>{new Date().toDateString()}</p>
         <p>{this.state.time}</p><br />
-        <p id="disp1">Start task: </p><br />
-        <p id="disp2">End task: </p><br />
-        <p id="disp3">Task waiting...</p>
+        <p>{this.state.disp1} </p><br />
+        <p id="disp2">{this.state.disp2} </p><br />
+        <p id="disp3" style={{ background: this.state.bgd }}>
+          {this.state.disp3}</p>
+
+        <audio controls
+          src="audio.wav" type="audio/mpeg" />
+
       </div>
     );
   }
