efb400882777573cd55bd788200ec43885f6387e
Claudiu <mhvsl@yahoo.com>
2019-07-13 20:11:47 +0300

    header

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ class Header extends Component {
   render() {
     return (
       <div>
-        <h1 className="bg-green white underline tc ma0">3h Timer</h1>
+        <h1 className="bg-green ma3">3 Hour Timer</h1>
       </div>
     );
   }
